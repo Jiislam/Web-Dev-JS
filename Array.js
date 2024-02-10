2b8fa73497dece32cf43cf8c350728b76f57ff16
Jiislam <jihadul.islam9043@gmail.com>
2024-02-10 16:36:46 +0600

    first commit

---
+++ Array.js
@@ -0,0 +1,2 @@
+var friendsAge = [458,45,122,35,487,45];
+console.log(friendsAge.length);
---
+++ ArrayIndex.js
@@ -0,0 +1,6 @@
+var newArray = [4,5,6,1,23,8,9,1,4];
+ // get element by index
+
+ console.log(newArray[1], newArray[5], newArray[0]);
+
+//  index numbers starts from 0, 0 means the first item of the array
