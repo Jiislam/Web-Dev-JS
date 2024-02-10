c8092166ab7f1cd3b605930a97906b6a58050d49
Jiislam <jihadul.islam9043@gmail.com>
2024-02-10 16:40:17 +0600

    first commit

--- ArrayIndex.js
+++ ArrayIndex.js
@@ -4,3 +4,10 @@ var newArray = [4,5,6,1,23,8,9,1,4];
  console.log(newArray[1], newArray[5], newArray[0]);
 
 //  index numbers starts from 0, 0 means the first item of the array
+
+// assigning values to current array index
+newArray[1] = 11;
+newArray[0] = 1;
+
+console.log(newArray);
+
