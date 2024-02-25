4e88ef878a3c04eb90bb4015070430a4f1fe68b8
Jiislam <jihadul.islam9043@gmail.com>
2024-02-25 16:14:20 +0600

    comparison

--- comaparison.js
+++ comaparison.js
@@ -4,3 +4,13 @@ console.log(5==6);
 console.log(5==5);
 
 console.log('5'===5);
+
+
+var num1 = 56;
+var num2= 60;
+console.log(num1< num2 );
+console.log(num1> num2 );
+console.log(num1<= num2 );
+console.log(num1>= num2 );
+console.log(num1 == num2 );
+console.log(num1 != num2 );
